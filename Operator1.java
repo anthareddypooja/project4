d4717b1102d87e0de8d5948dd9e0a4369b7a1cf2
vinod <anthareddypooja@gmail.com>
2021-10-12 13:55:27 +0530

    added Operator1.java

---
+++ Operator1.java
@@ -0,0 +1,17 @@
+class Operator1
+{
+    int a=12;b=14,c;
+    int add()
+    {
+        c=a+b;
+        System.out.println(c);
+    }
+}
+class test
+{
+    public static void main(String args[]
+    {
+        Operator1 op=new Operator1();
+        op.add();
+    })
+}
