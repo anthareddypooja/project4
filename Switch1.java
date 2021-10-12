6cc63bb39bdd67ff9e886820c2ca37e8e3ecebec
anthareddypooja <anthareddypooja@gmail.com>
2021-10-12 13:12:44 +0530

    added Switch1.java

---
+++ Switch1.java
@@ -0,0 +1,22 @@
+class Switch1
+{
+    public static void main(String args[])
+    {
+        String grade="A";
+        switch(grade)
+        {
+            case "A":
+            System.out.println("merit");
+            break;
+            case "B":
+            System.out.println("above avarage");
+            break;
+            case "C":
+            System.out.println("Avarage");
+            break;
+            case "D":
+            System.out.println("below avarage");
+            break;
+        }
+    }
+}
